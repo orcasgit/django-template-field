bb51831bfe2314377c5209472f76751205f075bf
Brad Pitcher <bradpitcher@gmail.com>
2016-01-11 15:16:54 -0800

    add setting to globally enable/disable rendering

--- templatefield/fields.py
+++ templatefield/fields.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.template import Context, Template
-from . import settings
+from .settings import get_setting
 
 
 class TemplateTextField(models.TextField):
@@ -9,13 +9,14 @@ class TemplateTextField(models.TextField):
         if value is None:
             return None
 
-        if not context.get('show_rendered'):
+        should_render = get_setting('TEMPLATE_FIELD_RENDER')
+        if not context.get('show_rendered') or not should_render:
             # `show_rendered` flag is unset, return the templeate
             return value
 
         # Return the rendered template
         template = Template(value)
         context_dict = {}
-        context_dict.update(settings.TEMPLATE_FIELD_CONTEXT)
+        context_dict.update(get_setting('TEMPLATE_FIELD_CONTEXT'))
         context_dict.update(context.get('tmpl_context', {}))
         return template.render(Context(context_dict))
--- templatefield/settings.py
+++ templatefield/settings.py
@@ -1,4 +1,17 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
-TEMPLATE_FIELD_CONTEXT = getattr(settings, 'TEMPLATE_FIELD_CONTEXT', {})
+def get_setting(setting):
+    """ Get the specified django setting, or it's default value """
+    defaults = {
+        # The context to use for rendering fields
+        'TEMPLATE_FIELD_CONTEXT': {},
+        # When this is False, don't do any TemplateField rendering
+        'TEMPLATE_FIELD_RENDER': True
+    }
+    try:
+        return getattr(settings, setting, defaults[setting])
+    except KeyError:
+        msg = "{0} is not specified in your settings".format(setting)
+        raise ImproperlyConfigured(msg)
